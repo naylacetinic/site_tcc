21cce11322a47f188e65c5958fb642fa26c6f377
Nayla Cetinic <nayla.cetinic.z@gmail.com>
2021-12-17 15:28:04 -0300

    Fotos, texto e design
    adição de fotos, textos, tabelas de horário, grid, exclui a parte da pesquisa no cabeçalho

---
+++ site-TCC/js/scipts.js
@@ -0,0 +1,26 @@
+function validar()  {
+    var nome = form1.nome.value;
+    var email = foorm1.email.value;
+    var senha = form1.senha.value;
+    var rep_senha = form1.rep_senha.value;
+
+    if (nome == "") {
+    alert ('Preencha o campo com seu nome')
+    form1.nome.focus();
+    return false
+    }
+    if (email == "") {
+    alert ('Preencha o campo com seu email')
+    form1.email.focus();
+    return false
+    }
+    if (senha == "") {
+    alert ('Preencha o campo com sua senha')
+    form1.senha.focus();
+    return false
+    }
+    if (nome.length <5) {
+        alert ('Digite seu nome completo')
+        return false
+    }
+}
